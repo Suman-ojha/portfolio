112291db682b8f7695226cba331a5d23461ff34b
Suman-ojha <sumanojha1999@gmail.com>
2023-03-11 02:51:27 +0530

    Updates

---
+++ precache-manifest.c7e12c7146724687ad9859cc4a777e3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6d942843a5a96bbb8f1",
+    "url": "/portfolio/static/css/main.ab23b00f.chunk.css"
+  },
+  {
+    "revision": "b6d942843a5a96bbb8f1",
+    "url": "/portfolio/static/js/main.b6d94284.chunk.js"
+  },
+  {
+    "revision": "aba3f5b8888cac36bd8f",
+    "url": "/portfolio/static/css/1.105299f2.chunk.css"
+  },
+  {
+    "revision": "aba3f5b8888cac36bd8f",
+    "url": "/portfolio/static/js/1.aba3f5b8.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "4d47e4197d271d108a19c72ea22ee606",
+    "url": "/portfolio/static/media/Suman Ojha_1 .4d47e419.pdf"
+  },
+  {
+    "revision": "ac6a41a570245dd8ba794f2d36144149",
+    "url": "/portfolio/static/media/three.ac6a41a5.svg"
+  },
+  {
+    "revision": "d3b5a350aa2d8fb7ade347dcca6b7451",
+    "url": "/portfolio/index.html"
+  }
+];
